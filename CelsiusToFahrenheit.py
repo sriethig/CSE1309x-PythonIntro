0bfda99690cd39bba0dfcb7b18d8ddf77c2f29d8
Sonja1 Riethig <ris38975@hs-regensburg.de>
2017-01-16 11:31:39 +0100

    first commit

---
+++ CelsiusToFahrenheit.py
@@ -0,0 +1,15 @@
+# input celsius
+celsius = 20
+
+# multiply by 9
+result = celsius * 9
+
+# divide by 5
+result = result / 5
+
+# add 32
+result = result + 32
+
+# show result
+fahrenheit = result
+print(fahrenheit)
